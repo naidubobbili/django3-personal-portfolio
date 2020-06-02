552e046d3922fcaf1445cb49f144b2ff080a1990
naidubobbii <naidubobbili373@gmail.com>
2020-06-02 09:29:43 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField(max_length=250000)
+    date=models.DateField()
+
+
+
+    def __str__(self):
+        return self.title
